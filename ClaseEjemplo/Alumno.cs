b0d7a4d883a3cfb9949f6040a7f874774a015d3d
Olstertecn11 <otzunund@miumg.edu.gt>
2023-04-18 19:09:23 -0600

    primera clase, ejemplos de metodos con programacion orientada a objetos

---
+++ ClaseEjemplo/Alumno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseEjemplo
+{
+    public class Alumno
+    {
+        public string nombre;
+        public string carnet;
+        public int semestre;
+
+        //Metodo constructor
+        public Alumno()
+        {
+            this.nombre = "Oliver";
+            this.carnet = "";
+            this.semestre = 0;
+        }
+
+        public Alumno(string nombre, string carnet, int s)
+        {
+            this.nombre = nombre;
+            this.carnet = carnet;
+            this.semestre = s;
+        }
+
+        public void imprimirDatos()
+        {
+            Console.WriteLine("Datos de alumno");
+            Console.WriteLine("Nombre: " + this.nombre);
+            Console.WriteLine("Carnet: " + this.carnet);
+            Console.WriteLine("Semestre: " + this.semestre);
+        }
+    }
+}
---
+++ ClaseEjemplo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//clases
+
+using ClaseEjemplo;
+
+Alumno alumno1 = new Alumno("Ana", "0901-20-10107", 2);
+
+alumno1.imprimirDatos();
+
+
+
+
+
+
+
+
+
+
