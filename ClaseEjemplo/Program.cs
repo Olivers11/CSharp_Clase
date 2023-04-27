efcb0c4017fce932206afc042064d966e63946e1
Oliver José Tzunún Dávila <olstertecn597@outlook.com>
2023-04-26 18:14:21 -0600

    Ejercicio #1 Tarea #1

--- ClaseEjemplo/Program.cs
+++ ClaseEjemplo/Program.cs
@@ -1,63 +1,33 @@
-﻿
+
 //clases
 
 using ClaseEjemplo;
 
-int tamanio = 0;
-Alumno[] alumnos;
-int opcion;
 
-Console.Write("Cuantos alumnos desea ingresar?: ");
-tamanio = Convert.ToInt32(Console.ReadLine());
-alumnos = new Alumno[tamanio];
 
-int posicion = 0;
+Console.Write("Ingrese el valor de N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string[] cadenas = new string[N];
 
-do
+
+for(int i = 0; i < N; i++)
 {
-    Console.Clear();
-    Console.WriteLine("1- Ingresar alumno");
-    Console.WriteLine("2- Ver Alumnos");
-    Console.WriteLine("3- Salir");
-    opcion = Convert.ToInt32(Console.ReadLine());
+    Console.Write("ingrese la cadena: ");
+    cadenas[i] = Console.ReadLine();
+}
 
-    if (opcion == 1)
+
+string mayor = "";
+// ar = { "Oliver", "Mar", "Guatemala" }
+for(int i = 0; i < N; i++)
+{
+    if (cadenas[i].Length > mayor.Length)
     {
-        Console.Clear();
-        if (posicion == tamanio)
-        {
-            Console.WriteLine("lista llena");
-            Console.ReadKey();
-        }
-        else
-        {
-            Console.Write("Nombre: ");
-            string nombre = Console.ReadLine();
-
-            Console.Write("Carnet: ");
-            string carnet = Console.ReadLine();
-
-            Console.Write("Semestre: ");
-            int semestre = Convert.ToInt32(Console.ReadLine());
-
-            alumnos[posicion] = new Alumno(nombre, carnet, semestre);
-            posicion++;
-            Console.WriteLine("Alumno ingresado correctamente :D");
-            Console.ReadKey(true);
-        }
+        mayor = cadenas[i];
     }
-    else if (opcion == 2)
-    {
-        Console.Clear();
-        Console.WriteLine("==== LISTADO ===");
-        for (int i = 0; i < posicion; i++)
-        {
-            alumnos[i].imprimirDatos();
-            Console.WriteLine();
-        }
-        Console.ReadKey(true);
-    }
-} while (opcion != 3);
+}
+
+Console.WriteLine("Cadena mas grande: " + mayor);
 
 
 
